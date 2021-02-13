4bd7f9d443186a33aeecc576ccef044b930a5d18
Scott Huchton <shuchton@gmail.com>
2021-02-13 10:35:29 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div><TextInput /></div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import TextInput from '/@/components/form/TextInput.vue'
+
+  export default defineComponent({
+    name: 'App',
+    components: {
+      TextInput,
+    },
+  })
+</script>
